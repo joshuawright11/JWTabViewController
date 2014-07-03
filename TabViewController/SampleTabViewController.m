12b757a97e5e175655a68956984f4ac9f86cdde1
Josh <joshuawright11@gmail.com>
2014-07-02 20:27:05 -0500

    Starting less than 3 tab edit

--- TabViewController/SampleTabViewController.m
+++ TabViewController/SampleTabViewController.m
@@ -19,13 +19,13 @@
     [super viewDidLoad];
     UIViewController *test1 = [[self storyboard] instantiateViewControllerWithIdentifier:@"Test"];
     UIViewController *test2 = [[self storyboard] instantiateViewControllerWithIdentifier:@"Test"];
-    UIViewController *test3 = [[self storyboard] instantiateViewControllerWithIdentifier:@"Test"];
-    UIViewController *test4 = [[self storyboard] instantiateViewControllerWithIdentifier:@"Test"];
-    UIViewController *test5 = [[self storyboard] instantiateViewControllerWithIdentifier:@"Test"];
-    UIViewController *test6 = [[self storyboard] instantiateViewControllerWithIdentifier:@"Test"];
+    //UIViewController *test3 = [[self storyboard] instantiateViewControllerWithIdentifier:@"Test"];
+    //UIViewController *test4 = [[self storyboard] instantiateViewControllerWithIdentifier:@"Test"];
+    //UIViewController *test5 = [[self storyboard] instantiateViewControllerWithIdentifier:@"Test"];
+    //UIViewController *test6 = [[self storyboard] instantiateViewControllerWithIdentifier:@"Test"];
     
-    [self putViewControllers:@[test1,test2,test3,test4,test5,test6]
-                  withTitles:@[@"1",@"2",@"3",@"4",@"5",@"6"]];
+    [self putViewControllers:@[test1,test2]//,test3,test4,test5,test6]
+                  withTitles:@[@"1",@"2"]];//,@"3",@"4",@"5",@"6"]];
 
     self.tabBarBackgroundColor = [UIColor blackColor];
     self.textColor = [UIColor grayColor];
